f7204b003b9cd5d5aef2a5092d6e43990d49de00
Adam Gulczyński <a.a.gulczynski@gmail.com>
2023-10-11 16:14:54 +0200

    add CRUD for cv profiles (#5)

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cv-backend-go
+
+## App built with Go 1.21
+
+### The app uses:
+- Postgres
+- Docker
+- golang-migrate
+- sqlc
+- testify
--- db/migrations/000001_init_schema.down.sql
+++ db/migrations/000001_init_schema.down.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS cv_profile;
-DROP TABLE IF EXISTS cv_education;
+DROP TABLE IF EXISTS cv_profiles;
+DROP TABLE IF EXISTS cv_educations;
 DROP TABLE IF EXISTS skills;
 DROP TABLE IF EXISTS projects;
